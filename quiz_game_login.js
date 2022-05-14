18fcdcae0d3c981eee4469f16ef576e90c8091e5
megalomaniaaa <96398377+megalomaniaaa@users.noreply.github.com>
2022-05-14 11:39:10 -0400

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,21 @@
+function adduser()
+{
+    player1_name = document.getElementById("player1_name_input").value
+    player2_name = document.getElementById("player2_name_input").value
+
+    localStorage.setItem("pLaYeR1!",player1_name)
+    localStorage.setItem("pLaYeR2@",player2_name)
+    window.location="game_page.html"
+}
+function send()
+{
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2)
+    question_number = "<br>Answer : <input type='text' id='input_check_box'> ";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()' >check<button>";
+    row = question_number + input_box + check_button ;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
